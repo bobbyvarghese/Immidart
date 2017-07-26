c850dcaf70f5e5342b05e5c1a9a91111db529a77
Nimisha Sreenivasan <nimisha.sreenivasan@sysfore.com>
2017-07-26 17:54:01 +0530

    added header and footer changed

--- wp-content/themes/immidart/footer.php
+++ wp-content/themes/immidart/footer.php
@@ -2,24 +2,24 @@
     <div class="container">
       <div class="row">
         <div class="col-sm-3">
-          <h6 class="footer-section-title">
+          <h6 class="footer-section-title foo">
 			 <?php if ( !function_exists( 'dynamic_sidebar' ) || !dynamic_sidebar('footer-left-widget') ) ?>
          </h6> 
         </div>
         <div class="col-sm-3">
-          <h6 class="footer-section-title">
+          <h6 class="footer-section-title foo">
 			<?php if ( !function_exists( 'dynamic_sidebar' ) || !dynamic_sidebar('footer-center-widget') ) ?>
 		  </h6>
           
         </div>
         <div class="col-sm-3">
-          <h6 class="footer-section-title">
+          <h6 class="footer-section-title foo">
 			  <?php if ( !function_exists( 'dynamic_sidebar' ) || !dynamic_sidebar('footer-right-widget') ) ?>
 			  </h6>
          <?php if ( !function_exists( 'dynamic_sidebar' ) || !dynamic_sidebar('footer-right-widget3') ) ?>
         </div>
         <div class="col-sm-3">
-          <h6 class="footer-section-title">
+          <h6 class="footer-section-title foo">
 			  <?php if ( !function_exists( 'dynamic_sidebar' ) || !dynamic_sidebar('footer-right-widget2') ) ?>
 			</h6>
         </div>
