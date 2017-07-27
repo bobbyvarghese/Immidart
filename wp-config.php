b8f46e8c53689346595eca7e1da743a26e625b2b
bobbyvarghese <bobby.varghese@sysfore.com>
2017-07-27 14:39:57 +0530

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define('DB_NAME', 'Immidart');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'Admin@201789');
+define('DB_PASSWORD', 'Admin@201789222');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
